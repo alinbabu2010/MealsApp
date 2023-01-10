84f0339a446329191f64fc3a010f53c2e98be232
Alin Babu <alinbabu2010@gmail.com>
2023-01-10 16:41:37 +0530

    Add meal details screen structure

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  static const routeName = 'meals_details';
+
+  const MealDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)?.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(mealId),
+      ),
+      body: Center(
+        child: Text("The Meal! $mealId"),
+      ),
+    );
+  }
+}
