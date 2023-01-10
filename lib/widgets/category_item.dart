1c2e9b56a4814152b0550422bbf8dfcadccfe186
Alin Babu <alinbabu2010@gmail.com>
2023-01-10 11:42:14 +0530

    Add navigation to categories meals screen.
    Add InkWell widget for click and ripple effect.

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -12,20 +13,33 @@ class CategoryItem extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => CategoriesMealsScreen(id, title),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [color.withOpacity(0.7), color],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(15)),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.titleMedium,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15)),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
