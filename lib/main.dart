d1bc250ee16b9d914a7572b784ac72141878c619
Alin Babu <alinbabu2010@gmail.com>
2023-01-10 11:02:30 +0530

    Add style and theme to app widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.pinkAccent,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData().textTheme.copyWith(
+              bodyMedium: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodySmall: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleMedium: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'Roboto Condensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
       debugShowCheckedModeBanner: false,
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -21,7 +21,10 @@ class CategoryItem extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
           borderRadius: BorderRadius.circular(15)),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
     );
   }
 }
