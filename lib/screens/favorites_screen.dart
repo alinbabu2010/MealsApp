0dbc6876c34ab52e47b143857def1c2551c1459f
Alin Babu <alinbabu2010@gmail.com>
2023-01-11 17:21:19 +0530

    Add favorite screen listView and empty favorites handling

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({Key? key}) : super(key: key);
+  final List<Meal> favoriteMeals;
+
+  const FavoritesScreen({Key? key, required this.favoriteMeals})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('The favorites'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('You have no favorites yet - add some!'),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          final meal = favoriteMeals[index];
+          return MealItem(
+            id: meal.id,
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            affordability: meal.affordability,
+            complexity: meal.complexity,
+            duration: meal.duration,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
